590ab554393dc50a2766ea149f129149e8e0794a
Michael Markovsky <misha.mar2001@gmail.com>
2025-07-24 17:51:13 +0300

    MODIFIED: url_check | Downloads the file

--- url_check.go
+++ url_check.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
@@ -13,5 +15,32 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	fmt.Println("Status code is:", resp.StatusCode)
+
+	// Check that status code is ok
+	if resp.StatusCode != http.StatusOK {
+		log.Fatalf("Bad status: %s", resp.Status)
+	} else {
+		fmt.Println("Status code is:", resp.StatusCode)
+	}
+
+	// Check content-type (whether its an image or zip file)
+	contentType := resp.Header.Get("Content-Type")
+	fmt.Println("Content-Type:", contentType)
+
+	//headerContent := resp.Header.Get("Content-Disposition")
+	//fmt.Println(resp.Header)
+
+	// Create output file
+	out, err := os.Create("exmaple.png")
+	if err != nil {
+		log.Fatal("File creation failed:", err)
+	}
+	defer out.Close()
+
+	// Copy response body to file
+	n, err := io.Copy(out, resp.Body)
+	if err != nil {
+		log.Fatal("Copy failed:", err)
+	}
+	fmt.Printf("Downloaded %d bytes\n", n)
 }
