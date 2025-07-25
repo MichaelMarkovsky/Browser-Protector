2a6e29dee4de42e26e747f53e7e57a40bb33ee81
Michael Markovsky <misha.mar2001@gmail.com>
2025-07-25 16:13:10 +0300

    MODIFIED: downloads the url, dynamic filename

--- url_check.go
+++ url_check.go
@@ -4,14 +4,20 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"mime"
 	"net/http"
+	"net/url"
 	"os"
+	"path"
 )
 
 func main() {
+
+	URL := "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMD8qCrtyprMTaONWq2gaQxyXr7fR4WKUW1Q&s"
+
 	// err - stores any error
 	// resp - the response of the get request
-	resp, err := http.Get("[URL]")
+	resp, err := http.Get(URL)
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -27,11 +33,27 @@ func main() {
 	contentType := resp.Header.Get("Content-Type")
 	fmt.Println("Content-Type:", contentType)
 
-	//headerContent := resp.Header.Get("Content-Disposition")
-	//fmt.Println(resp.Header)
+	// Get the file name
+	filename := ""
+	// Try to extract the filename from disposition (if a file is meant to be downloaded it most likey has a 'Content-Dosposition' section in the header)
+	disposition, params, err := mime.ParseMediaType(resp.Header.Get("Content-Disposition"))
+	fmt.Println(disposition)
+	if err != nil {
+		// If the file has no disposition section, the file could have the name from the url itself, extracting from the url
+		fmt.Println("There is no disposition in the header,\nUsing the name from the url parsed:")
+
+		u, _ := url.Parse(URL)
+		cleanPath := u.Path
+		filename = path.Base(cleanPath)
+		fmt.Println("filename:", filename)
+
+	} else {
+		filename = params["filename"]
+		fmt.Println("filename:", filename)
+	}
 
 	// Create output file
-	out, err := os.Create("exmaple.png")
+	out, err := os.Create(filename)
 	if err != nil {
 		log.Fatal("File creation failed:", err)
 	}
