e2665c13822990b66c9e5a909ab18bd4caa64126
Michael Markovsky <misha.mar2001@gmail.com>
2025-07-26 19:24:37 +0300

    MODIFIED: if file compressed, uncompress

--- url_check.go
+++ url_check.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"archive/zip"
 	"fmt"
 	"io"
 	"log"
@@ -9,6 +10,7 @@ import (
 	"net/url"
 	"os"
 	"path"
+	"strings"
 )
 
 func main() {
@@ -32,6 +34,12 @@ func main() {
 	// Check content-type (whether its an image or zip file)
 	contentType := resp.Header.Get("Content-Type")
 	fmt.Println("Content-Type:", contentType)
+
+	IsZip := false
+	if strings.Contains(contentType, "zip") {
+		IsZip = true
+		fmt.Println("Zip file found.")
+	}
 
 	// Get the file name
 	filename := ""
@@ -69,6 +77,52 @@ func main() {
 		fmt.Printf("Downloaded %d bytes\n", n)
 	} else {
 		log.Fatal("File name has not been located, download failed.")
+	}
+
+	// If the file is of zip type then unzip (if its compressed then uncompress)
+	if IsZip {
+
+		// Open a zip archive for reading.
+		r, err := zip.OpenReader(filename)
+		if err != nil {
+			log.Fatalf("impossible to open zip reader: %s", err)
+		}
+		defer r.Close()
+
+		// Iterate through the files in the archive,
+		for k, f := range r.File {
+			fmt.Printf("Unzipping %s:\n", f.Name)
+			rc, err := f.Open()
+			if err != nil {
+				log.Fatalf("impossible to open file n°%d in archine: %s", k, err)
+			}
+			defer rc.Close()
+			// define the new file path
+			newFilePath := fmt.Sprintf("uncompressed/%s", f.Name)
+
+			// CASE 1 : we have a directory
+			if f.FileInfo().IsDir() {
+				// if we have a directory we have to create it
+				err = os.MkdirAll(newFilePath, 0777)
+				if err != nil {
+					log.Fatalf("impossible to MkdirAll: %s", err)
+				}
+				// we can go to next iteration
+				continue
+			}
+
+			// CASE 2 : we have a file
+			// create new uncompressed file
+			uncompressedFile, err := os.Create(newFilePath)
+			if err != nil {
+				log.Fatalf("impossible to create uncompressed: %s", err)
+			}
+			_, err = io.Copy(uncompressedFile, rc)
+			if err != nil {
+				log.Fatalf("impossible to copy file n°%d: %s", k, err)
+			}
+		}
+
 	}
 
 }
