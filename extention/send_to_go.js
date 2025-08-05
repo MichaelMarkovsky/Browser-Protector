80a4e6687dcb14be585477d6ead3931271b4b0e5
Michael Markovsky <misha.mar2001@gmail.com>
2025-08-05 15:59:59 +0300

    CREATED: file created

---
+++ extention/send_to_go.js
@@ -0,0 +1,25 @@
+async function sendDataToGo(id,url,filename,mime) {
+        const data = {
+            id: id,
+            url:url,
+            filename:filename,
+            mime:mime
+        };
+
+        try {
+            const response = await fetch('http://localhost:8080/submit-data', { 
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            });
+
+            const result = await response.text();
+            console.log(result); // should log: Data received successfully!
+        } catch (error) {
+            console.error('Error:', error);
+        }
+    }
+
+    sendDataToGo();
