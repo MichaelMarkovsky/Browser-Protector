66b9f5f93b01a4c0a4b4fc0aaac5f36d87f73889
Michael Markovsky <misha.mar2001@gmail.com>
2025-08-05 16:00:48 +0300

    MODIFIED: passes information to send_to_go.js

--- extention/service-worker.js
+++ extention/service-worker.js
@@ -1,7 +1,13 @@
+import {sendDataToGo} from './send_to_go.js';
+
+
 chrome.downloads.onCreated.addListener(downloadItem => {
   console.log("New download started:");
   console.log("ID:", downloadItem.id);
   console.log("URL:", downloadItem.url);
   console.log("Filename:", downloadItem.filename);
   console.log("Mime type:", downloadItem.mime);
+
+  //send the data to golang server via the function
+  sendDataToGo(downloadItem.id,downloadItem.url,downloadItem.filename,downloadItem.mime)
 });
