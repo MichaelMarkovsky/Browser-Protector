a3a9085c00d90fef8ff2f40906fe61f4b02ec8cb
Michael Markovsky <misha.mar2001@gmail.com>
2025-08-06 15:47:15 +0300

    MODIFIED: get data from receive.go

--- main.go
+++ main.go
@@ -1,5 +1,24 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
-	receive()
+
+	// create channel to receive data
+	dataChan := make(chan DataPayload)
+
+	// run the server in a goroutine
+	go receive(dataChan)
+
+	// wait for data to come from handler
+	fmt.Println("Waiting for data...")
+
+	for {
+		payload := <-dataChan // blocks until data arrives
+		fmt.Println("Main received:", payload)
+
+		fmt.Printf("Received id: %d, download_url: %s, mime: %s\n", payload.ID, payload.URL, payload.MIME)
+	}
 }
