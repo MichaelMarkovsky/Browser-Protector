1aa59f50799cb78255c84f4bb552fb954d50a2b1
Michael Markovsky <misha.mar2001@gmail.com>
2025-08-06 16:25:34 +0300

    MODIFIED: sends url to url_check.go

--- main.go
+++ main.go
@@ -20,5 +20,9 @@ func main() {
 		fmt.Println("Main received:", payload)
 
 		fmt.Printf("Received id: %d, download_url: %s, mime: %s\n", payload.ID, payload.URL, payload.MIME)
+
+		fmt.Println("Sending link to Virus Total...")
+
+		url_check(payload.URL)
 	}
 }
